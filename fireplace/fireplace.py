d62950cb338834cfd1d59a778123567a8547c503
Sebastian Brannstrom <teknolog@gmail.com>
2017-12-15 22:22:41 -0800

    WIP

--- fireplace/fireplace.py
+++ fireplace/fireplace.py
@@ -11,12 +11,12 @@ class Fireplace(object):
 
     def set_flame(self, target_flame_level):
         for _ in range(0, target_flame_level - self._flame_level):
-            self._hardware_io.toggle_onoff()
+            self._hardware_io.toggle_flame()
     
         self._flame_level = target_flame_level
 
     def set_fan(self, target_fan_level):
         for _ in range(0, target_fan_level - self._fan_level):
-            self._hardware_io.toggle_flame()
+            self._hardware_io.toggle_fan()
     
         self._fan_level = target_fan_level
--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from fireplace.gpioio import GPIOIO
 def main():
     parser = argparse.ArgumentParser(description='Fireplace management tool')
     parser.add_argument('command', choices=['onoff', 'flame', 'fan'])
-    # parser.add_argument('--value', dest='accumulate', action='store_const',
-    #                 const=sum, default=max,
-    #                 help='sum the integers (default: find the max)')
+    parser.add_argument('--value', help='set the value', type=int)
 
     args = parser.parse_args()
 
@@ -19,9 +17,10 @@ def main():
     if args.command == 'onoff':
         fireplace.toggle_onoff()
     elif args.command == 'flame':
-        fireplace.toggle_flame()
+        fireplace.set_flame(args.value)
     elif args.command == 'fan':
-        fireplace.toggle_fan()    
+        fireplace.set_fan(args.value)    
+
 
 if __name__ == '__main__':
     main()
