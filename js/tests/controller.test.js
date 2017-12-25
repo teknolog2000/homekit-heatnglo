17d6a2626806c8e4676f04ed308c7ebc74ef4c09
Sebastian Brannstrom <teknolog@gmail.com>
2017-12-25 08:31:30 +0100

    Progress

--- js/tests/controller.test.js
+++ js/tests/controller.test.js
@@ -1,19 +1,10 @@
 var test = require('tape');
-var FirePlaceController = require('../controller.js');
+var FireplaceController = require('../controller.js').FireplaceController;
 
-test('timing test', function (t) {
-    t.plan(2);
-
-    t.equal(typeof Date.now, 'function');
-    var start = Date.now();
-
-    setTimeout(function () {
-        t.equal(Date.now() - start, 100);
-    }, 100);
-});
-
-test('should return -1 when the value is not present in Array', function (t) {
-    t.equal(-1, [1, 2, 3].indexOf(4)); // 4 is not present in this array so passes
+test('instantiate the controller', function (t) {
+    t.doesNotThrow(function () {
+        console.log(FireplaceController);
+        FireplaceController.init();
+    });
     t.end();
 });
-
